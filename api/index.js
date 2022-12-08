fd1d1c4868eb8bfbe00a6a08b1c7a1c0d522a69a
Phil Skaroulis <pskaroulis@oreilly.com>
2022-12-08 09:01:39 -0500

    clean up

--- api/index.js
+++ api/index.js
@@ -4,30 +4,30 @@ const port = 3000
 
 // read .env only in dev
 if (process?.env?.NODE_ENV !== "production") {
-  require("dotenv").config({ path: __dirname + '/.env' });
+  require("dotenv").config({ path: __dirname + '/.env' })
 }
 
 app.get('/', (req, res) => {
-  const { id } = req.query;
+  const { id } = req.query
 
   const db = require("./datasource")
 
-  const docRef = db.collection("MyName").doc(id);
+  const docRef = db.collection("MyName").doc(id)
 
   docRef.get()
     .then((doc) => {
       if (doc.exists) {
-        const { firstName, lastName } = doc.data();
-        res.status(200).send(`Hello. My name is ${firstName} ${lastName}.`);
+        const { firstName, lastName } = doc.data()
+        res.status(200).send(`Hello. My name is ${firstName} ${lastName}.`)
       } else {
-        console.error("id not found");
-        res.status(401).json(result);
+        console.error("id not found")
+        res.status(401).json(result)
       }
     })
     .catch((error) => {
-      console.error(error);
-      res.status(401).json(result);
-    });
+      console.error(error)
+      res.status(401).json(result)
+    })
 
 })
 
